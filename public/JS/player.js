c10d475068a8e5bded95fbf225f86cc6c8f480d9
Nikita Egorov <egorovnikita2002@gmail.com>
2023-03-20 15:36:09 +0100

    added changes to player

--- public/JS/player.js
+++ public/JS/player.js
@@ -1,13 +1,28 @@
 "use strict";
-const like = document.querySelector('.like');
-like.addEventListener('click', () => {
-});
-let playing = false;
-const playPauseButton = document.querySelector('.play-pause');
-playPauseButton.addEventListener('click', function () {
-    this.classList.toggle('playing');
-});
 const volumeSlider = document.getElementById('volumeSlider');
 volumeSlider.addEventListener('input', () => {
     const volumeValue = volumeSlider.value;
+});
+const like = document.getElementsByClassName('like')[0];
+const path = document.getElementById('path');
+let likeCondition = false;
+like.addEventListener('click', () => {
+    switch (likeCondition) {
+        case false:
+            like.style.width = '19';
+            like.style.height = '17';
+            path.style.stroke = 'none';
+            like.style.fill = 'green';
+            likeCondition = true;
+            like.classList.remove('hover');
+            break;
+        case true:
+            like.style.width = '17';
+            like.style.height = '15';
+            path.style.stroke = 'lightgrey';
+            like.style.fill = 'none';
+            likeCondition = false;
+            like.classList.add('hover');
+            break;
+    }
 });
--- public/TS/player.ts
+++ public/TS/player.ts
@@ -1,19 +1,30 @@
-const like = document.querySelector('.like') as SVGElement;
-
-like.addEventListener('click', () => {
-
-})
-
-let playing = false;
-
-const playPauseButton = document.querySelector('.play-pause') as HTMLDivElement;
-
-playPauseButton.addEventListener('click', function () {
-    this.classList.toggle('playing');
-});
-
 const volumeSlider = document.getElementById('volumeSlider') as HTMLInputElement;
 
 volumeSlider.addEventListener('input', () => {
   const volumeValue = volumeSlider.value;
 });
+
+const like = document.getElementsByClassName('like')[0] as SVGElement;
+const path = document.getElementById('path') as unknown as SVGPathElement;
+let likeCondition = false;
+like.addEventListener('click', () => {
+  switch(likeCondition) {
+    case false: 
+    like.style.width = '19'
+    like.style.height = '17'
+    path.style.stroke = 'none';
+    like.style.fill = 'green';
+    likeCondition = true;
+    like.classList.remove('hover')
+    break;
+    case true:
+    like.style.width = '17'
+    like.style.height = '15'
+    path.style.stroke = 'lightgrey'
+    like.style.fill = 'none';
+    likeCondition = false;
+    like.classList.add('hover')
+    break;
+  }
+}
+)
