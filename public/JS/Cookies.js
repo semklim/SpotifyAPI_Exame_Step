cf5b075a05cd22df12ea74108efe0e041715629b
Roman Klymashenko <romanklimashenko@gmail.com>
2023-04-05 13:36:55 +0300

    correct Cookies file

--- public/JS/Cookies.js
+++ public/JS/Cookies.js
@@ -11,9 +11,7 @@ class Cookie {
             let time = new Date();
             const now = time.getUTCDate();
             time.setUTCDate(now + days);
-            // @ts-ignore
-            time = time.toGMTString();
-            expires = `expires=${time}`;
+            expires = `expires=${time.toUTCString()}`;
         }
         else
             expires = `expires=`;
@@ -40,7 +38,7 @@ class Cookie {
  */
     static clearAllCookie() {
         let cookies = document.cookie.split("; ");
-        for (let c = 0; c < cookies.length; c++) {
+        for (let c = 0; c < cookies.length; c += 1) {
             let d = window.location.hostname.split(".");
             while (d.length > 0) {
                 let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
