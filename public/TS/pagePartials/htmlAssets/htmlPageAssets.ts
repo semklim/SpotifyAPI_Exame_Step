d339726c5ea7819b01ee22039b6be567eed49a09
Roman Klymashenko <romanklimashenko@gmail.com>
2023-04-07 15:41:01 +0300

    fix mistake.

--- public/TS/pagePartials/htmlAssets/htmlPageAssets.ts
+++ public/TS/pagePartials/htmlAssets/htmlPageAssets.ts
@@ -268,7 +268,7 @@ class Assets {
 		</div>
 		<div class="play-favorite-track-box"  style="background-color: ${color};">
 		  <div class="play-favorite-track">
-			<button class="play-favorite-track__button" href= "${obj.href}">
+			<button class="play-favorite-track__button" href= "${obj.id}">
 			  <svg role="img" height="28" width="28" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon" class="play-favorite-track__button--img" fill="black">
 				<path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z">
 				</path>
