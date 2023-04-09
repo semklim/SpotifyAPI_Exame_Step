27d275c8e117eb0b13724645b59a9336a7b1309d
Roman Klymashenko <romanklimashenko@gmail.com>
2023-04-09 14:02:54 +0300

    Fix bug with playing tracks in history page

--- public/JS/OnPlayFunc.js
+++ public/JS/OnPlayFunc.js
@@ -5,6 +5,8 @@ import { pauseConditionChange } from "./player.js";
 import { currentAudio } from "./player.js";
 import { favTracks } from "./main.js";
 import { favTracksDeleter } from "./main.js";
+import API from "./API.js";
+import prepareTracks from "./helpers/tracks/prepareTracksObj.js";
 export function findObjectByParam(array, value, anotherArray) {
     // favTracks =
     for (let i = 0; i < array.length; i += 1) {
@@ -15,8 +17,11 @@ export function findObjectByParam(array, value, anotherArray) {
             console.log(i);
             return i;
         }
-        else if (anotherArray[i].track.preview_url === value) {
-            return i;
+        else if (anotherArray) {
+            // @ts-ignore
+            if (anotherArray[i].track.preview_url === value) {
+                return i;
+            }
         }
     }
 }
@@ -26,27 +31,6 @@ export function OnPlayFunc(tracks) {
     if (tracks) {
         tracksObj = tracks;
         favTracksDeleter();
-        // playBtnSVG.innerHTML = '<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 gQUQL"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>';
-        // pauseConditionChange();
-        // if (playingAudio != null) {
-        //     playingAudio!.currentTime = 0
-        //     playingAudio!.volume = 0;
-        //     playingAudio!.pause()
-        //     playingAudio = null
-        // }
-        // if (currentAudio) {
-        //     currentAudio.pause();
-        // }
-        // const target = (event!.target as HTMLElement);
-        // if (target.className === "trackPlayBtn") {
-        //     const url = target.getAttribute('href');
-        //     if (url !== 'null') {
-        //         //@ts-ignore
-        //         playingAudio = onPlay(tracks, findObjectByParam(tracks, url));
-        //     } else {
-        //         console.log('Sorry track is not found')
-        //     }
-        // }
     }
     else {
         let tracks = tracksObj;
@@ -70,8 +54,22 @@ export function OnPlayFunc(tracks) {
                 tracks = favTracks;
             }
             if (url !== 'null') {
-                //@ts-ignore
-                playingAudio = onPlay(tracks, findObjectByParam(tracks, url, favTracks));
+                const index = findObjectByParam(tracks, url, favTracks);
+                if (index && index !== 0) {
+                    //@ts-ignore
+                    playingAudio = onPlay(tracks, index);
+                }
+                else {
+                    const refreshPlaylist = async () => {
+                        const playlistId = document.querySelector('.play-favorite-track__button').getAttribute('data-playlist-id');
+                        const playlist = await API.GetPlaylist(playlistId);
+                        const tracksObj = await prepareTracks(playlist, API);
+                        tracks = tracksObj;
+                        // @ts-ignore
+                        playingAudio = onPlay(tracks, findObjectByParam(tracks, url, favTracks));
+                    };
+                    refreshPlaylist();
+                }
             }
             else {
                 console.log('Sorry track is not found');
--- public/TS/main.ts
+++ public/TS/main.ts
@@ -80,15 +80,9 @@ const APP = (function (API, UI) {
 	const tracksByPlaylist = async (id: string) => {
 		const playlist = await API.GetPlaylist(id);
 		const tracks = await prepareTracks(playlist, API);
-
-		// end of logic
+		
 		UI.createTracks(playlist);
-
-		// const mainbox = document.querySelector('.favorite-tracks-contents');
-		// mainbox?.addEventListener('click', (e: Event) => {
 		OnPlayFunc(tracks);
-		// });
-
 	}
 
 	const PageSearch = async () => {
