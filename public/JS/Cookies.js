66bce8d73af38aafc0220ff6e3c39013d17ba0c0
Roman Klymashenko <romanklimashenko@gmail.com>
2023-04-12 02:27:01 +0300

    correct work of Cookies

--- public/JS/Cookies.js
+++ public/JS/Cookies.js
@@ -38,19 +38,22 @@ class Cookie {
  */
     static clearAllCookie() {
         let cookies = document.cookie.split("; ");
+        // for (let c = 0; c < cookies.length; c += 1) {
+        // 	let d = window.location.hostname.split(".");
+        // 	while (d.length > 0) {
+        // 		let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+        // 		let p = location.pathname.split('/');
+        // 		document.cookie = cookieBase + '/';
+        // 		while (p.length > 0) {
+        // 			document.cookie = cookieBase + p.join('/');
+        // 			p.pop();
+        // 		};
+        // 		d.shift();
+        // 	}
+        // }
         for (let c = 0; c < cookies.length; c += 1) {
-            let d = window.location.hostname.split(".");
-            while (d.length > 0) {
-                let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
-                let p = location.pathname.split('/');
-                document.cookie = cookieBase + '/';
-                while (p.length > 0) {
-                    document.cookie = cookieBase + p.join('/');
-                    p.pop();
-                }
-                ;
-                d.shift();
-            }
+            const name = cookies[c].split(";")[0].split("=")[0];
+            Cookie.set(name, '', -1);
         }
     }
 }
