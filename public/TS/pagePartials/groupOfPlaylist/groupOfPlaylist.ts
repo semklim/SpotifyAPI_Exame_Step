9e10de8e54c17bf6be516401fe1300f4ff0d9ce2
Roman Klymashenko <romanklimashenko@gmail.com>
2023-04-12 00:19:31 +0300

    correct groupOfPlaylist

--- public/TS/pagePartials/groupOfPlaylist/groupOfPlaylist.ts
+++ public/TS/pagePartials/groupOfPlaylist/groupOfPlaylist.ts
@@ -29,7 +29,7 @@ const listOfPlaylists = (list: object[], listLength: number) => {
 						<div class="imgBox__img">
 							<img aria-hidden="false" draggable="false" loading="lazy" src="${img}" alt="">
 							<div class="imgBox__PlayButton">
-								<button class="playBtn" aria-label="Play ${name}">
+								<button class="play-favorite-track__button playBtn" data-playlist-id="${id}" aria-label="Play ${name}">
 									<span class="playBtn__body">
 										<span class="iconWrapper" aria-hidden="true">
 											<svg role="img" height="24" width="24" aria-hidden="true" viewBox="0 0 24 24" class="SvgPlay"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
