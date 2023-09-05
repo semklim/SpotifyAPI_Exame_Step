ef257f2cf91e6abc1f3642530b70662c3c0b5d17
Roman Klymashenko <romanklimashenko@gmail.com>
2023-09-05 11:01:22 +0300

    starting

--- public/SPOTIFY_APP_KEYS/KEYS.js
+++ public/SPOTIFY_APP_KEYS/KEYS.js
@@ -1,7 +1,7 @@
-const clientId = 'Your CLIENT_ID';
-const clientSecret = 'Your CLIENT_SECRET';
+const clientId = '797c5434ef7543e7aafc871377f6d903';
+const clientSecret = '8f841f9365ee462384cad668edcec388';
 
 export {
-	clientId,
-	clientSecret
+  clientId,
+  clientSecret
 };
